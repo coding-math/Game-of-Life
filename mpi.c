997d47df8d96fcf7da8b53c03cae4be54816019b
matheuxito <matheus.souza28042001@gmail.com>
2023-11-29 20:31:31 -0300

    feat: add mpi version of game of life

--- mpi.c
+++ mpi.c
@@ -3,80 +3,65 @@
 // MATHEUS DE SOUZA MEIRE 140317
 // BRUNO PIRES 139892
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <locale.h>
-#include <omp.h>
-#include "mpi.h"
+#include <stdlib.h>
+#include <mpi.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
 
-// Vamos desenvolver o jogo da vida de Conway
-// Toda célula possui 8 outras vizinhas (horizontal, vertical e diagonal)
-
-// Cada célula está em um dos estados VIVA(1) ou MORTA(0)
-// Uma geracao é um conjunto de estados das células do tabuleiro
-// As sociedades evoluem com a atualização do tabuleiro
-
-// *** Regras ***
-// 1. Celulas vivas com menos de 2(dois) vizinhas morrem por abandono
-// 2. Cada célula viva com 2(dois) ou 3(três) deve permanecer viva para a próxima geração
-// 3. Cada célula viva com 4(quatro) ou mais vizinhos morre por superpopulação
-// 4. Cada célula morta com exatamente 3(três) vizinhos deve se tornar viva
- 
-// O que vamos fazer será considerando a versão Rainbow Game of Life
-// Possuindo a diferença de que: as novas celulas que tornam-se vivas deverão ter como valor
-// a média aritimética dos valores na vizinhança imediata. Portanto, as células vivas
-// tem valor maior que zero
-
-// Vamos programar um tabuleiro infinito, ou seja, a fronteira esquerda liga-se com a direita
-// e a fronteira superior liga-se com a inferior
-
-#define VIVA 1.0
-#define MORTA 0.0
-#define GERACOES 2000
+#define V 0        // versão: game of life 0, high life 1
+#define N 2048     // tamanho da matriz
 #define TAMANHO 2048
-#define MASTER 0
+#define VIVA 1
+#define MORTA 0
+#define G 2000     // número de gerações
 
-void join(float **grid, float *recvbuffer, int nRows) {
+void zeros(int **matrix, int nRows) {
   int i, j;
-  for (i = 0; i < nRows+2; i++) {
-    for (j = 0; j < TAMANHO; j++) {
-      if (i == 0 || i == nRows+1) grid[i][j] = 0;
-      else grid[i][j] = recvbuffer[((i - 1) * TAMANHO) + j];
+  for (i = 0; i < nRows; i++) {
+    for (j = 0; j < N; j++) {
+      matrix[i][j] = 0;
     }
   }
 }
 
-void inicializaGrid(float **grid){
-    for (int i = 0; i < TAMANHO; i++){
-        grid[i] = (float *)malloc(TAMANHO * sizeof(float));
-        for (int j = 0; j < TAMANHO; j++){
-            grid[i][j] = MORTA;
-        }
+void join(int **grid, int *recvbuffer, int nRows) {
+  int i, j;
+  for (i = 0; i < nRows+2; i++) {
+    for (j = 0; j < N; j++) {
+      if (i == 0 || i == nRows+1) grid[i][j] = 0;
+      else grid[i][j] = recvbuffer[((i - 1) * N) + j];
     }
+  }
 }
 
-void estadoInicial(float **grid){
+void setInitGrid(int *grid) {
+  int lin, col;
 
-    // com a figura de um Glider
-    int lin = 1, col   = 1;
-    grid[lin  ][col+1]  = VIVA;
-    grid[lin+1][col+2] = VIVA;
-    grid[lin+2][col ]  = VIVA;
-    grid[lin+2][col+1] = VIVA;
-    grid[lin+2][col+2] = VIVA;
-    
-    // Figura de um R-pentomino
-    lin = 10; col = 30;
-    grid[lin  ][col+1] = VIVA;
-    grid[lin  ][col+2] = VIVA;
-    grid[lin+1][col  ] = VIVA;
-    grid[lin+1][col+1] = VIVA;
-    grid[lin+2][col+1] = VIVA;
+  for (lin = 0; lin < N; lin++) {
+    for (col = 0; col < N; col++) {
+      grid[(lin * N) + col] = 0;
+    }
+  }
+  // GLIDER
+  lin = 1, col = 1;
+  grid[(lin * N) + col + 1] = 1;
+  grid[((lin + 1) * N) + col + 2] = 1;
+  grid[((lin + 2) * N) + col] = 1;
+  grid[((lin + 2) * N) + col + 1] = 1;
+  grid[((lin + 2) * N) + col + 2] = 1;
+
+  //R-pentomino
+  lin = 10; col = 30;
+  grid[(lin * N) + col + 1] = 1;
+  grid[(lin * N) + col + 2] = 1;
+  grid[((lin + 1) * N) + col] = 1;
+  grid[((lin + 1) * N) + col + 1] = 1;
+  grid[((lin + 2) * N) + col + 1] = 1;
 }
 
-
-// funcao que retorna a quantidade de vizinhos vivos em cada celula na posicao i,j
-int getNeighbors(float ** grid, int i, int j){
+int getNeighbors(int ** grid, int i, int j){
     int neighbors = 0;
 
     if (grid[ ((i - 1) + TAMANHO) % TAMANHO][j  ] == VIVA) neighbors++;
@@ -93,40 +78,26 @@ int getNeighbors(float ** grid, int i, int j){
     return neighbors;
 }
 
-int quantidadeCelulasVivas(float **grid){
-    int celulasVivas = 0;
-//#pragma omp parallel for shared(grid) reduction(+:celulasVivas)
-    for (int i = 0; i < TAMANHO; i++){
-        for (int j = 0; j < TAMANHO; j++){
-            if (grid[i][j] == VIVA){
-                celulasVivas++;
-            }
-        }
+void updateNeighbors(int **grid, int **newgrid, int i, int j, int neighbors) {
+  if (grid[i][j]) { // celula viva
+    if (neighbors == 2 || neighbors == 3) newgrid[i][j] = 1;
+    else newgrid[i][j] = 0;
+  }
+  else { //celula morta
+    if (V == 0) { // Game of life
+      if (neighbors == 3) newgrid[i][j] = 1;
+      else newgrid[i][j] = 0;
+    } else if (V == 1) { // High life
+      if (neighbors == 3 || neighbors == 6) newgrid[i][j] = 1;
+      else newgrid[i][j] = 0;
     }
-    return celulasVivas;
+  }
 }
 
-void updateNeighbors(float **grid, float **new_grid, int i, int j, int neighbors) {
-    float media_aritmetica = 0.0;
-    if (grid[i][j] == VIVA) {  // Célula Viva
-        if (neighbors < 2 || neighbors > 3){
-            new_grid[i][j] = MORTA;
-        }
-        else{ 
-            new_grid[i][j] = VIVA;
-        }
-    } else { // Célula Viva
-        if (neighbors == 3){
-            media_aritmetica = (float)neighbors/8.0;
-            new_grid[i][j] = VIVA;
-        }
-    }
-}
-
-int findLivingGenerations(float **grid, float **newgrid, int nRows) {
+int findLivingGenerations(int **grid, int **newgrid, int nRows) {
   int i, j, neighbors, livingGenerations = 0;
   for (i = 1; i < nRows+1; i++) {
-    for (j = 0; j < TAMANHO; j++) {
+    for (j = 0; j < N; j++) {
       neighbors = getNeighbors(grid, i, j);
       updateNeighbors(grid, newgrid, i, j, neighbors);
       if (newgrid[i][j]) livingGenerations++;
@@ -135,125 +106,100 @@ int findLivingGenerations(float **grid, float **newgrid, int nRows) {
   return livingGenerations;
 }
 
-void salvarMatriz(float **grid, FILE *arquivo) {
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
-            fprintf(arquivo, "%d ", (int)grid[i][j]);
-        }
-        fprintf(arquivo, "\n");
-    }
-    fprintf(arquivo, "\n");
-}
+int main(int argc, char **argv) {
+  int procID, nProcs, master = 0;
+  int i, j, generation, livingGenerations;
 
-int main(int argc, char **argv){
-    int processId; /* rank dos processos */
-    int noProcesses; /* Número de processos */
-    int nameSize; /* Tamanho do nome */
-    char computerName[MPI_MAX_PROCESSOR_NAME];
-    int i;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+  MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &noProcesses);
-    MPI_Comm_rank(MPI_COMM_WORLD, &processId);
-    //printf("processId %d chegou aqui131231231211\n", processId);
-    if ((int)processId == 0) {
-        //printf("chegou aquimasterrr\n");
-    }
-    //printf("processId %d passou do if\n", processId);
-    
-    double starT = MPI_Wtime(); // Tempo inicial da execução do programa
+  if(procID == master) {
+    printf("Game of Life - MPI\n");
+    printf("Rodando com %d processos...\n\n", nProcs);
+  }
 
-    ///////////////////////////////
+  double tempoInicio = MPI_Wtime(); //Tempo inicial da execução do programa
 
-    int nRows, localSize, vecRows[noProcesses];
-    int sendcounts[noProcesses], begin[noProcesses];
+  int nRows, localSize, vecRows[nProcs];
+  int sendcounts[nProcs], begin[nProcs];
 
-    for (i = 0; i < noProcesses; i++) {
-        localSize = TAMANHO/noProcesses;
-        begin[i] = i * localSize;
+  for (i = 0; i < nProcs; i++) {
+    localSize = N/nProcs;
+    begin[i] = i * localSize;
 
-        if (i == noProcesses-1) vecRows[i] = TAMANHO - begin[i];
-        else vecRows[i] = TAMANHO / noProcesses;
+    if (i == nProcs-1) vecRows[i] = N - begin[i];
+    else vecRows[i] = N/nProcs;
 
-        sendcounts[i] = vecRows[i] * TAMANHO;
-        if (i > 0) begin[i] = (vecRows[i-1] * TAMANHO * i);
-    }
-    if (processId == MASTER) {
-       //printf("chegou aqui111");
-    }
+    sendcounts[i] = vecRows[i] * N;
+    if (i > 0) begin[i] = (vecRows[i-1] * N * i);
+  }
 
-    nRows = vecRows[processId];
+  nRows = vecRows[procID];
 
-    float *recvbuffer = (float *) malloc(nRows * TAMANHO * sizeof(float));
+  int *recvbuffer = (int *) malloc(nRows * N * sizeof(int));
 
-    // Compartilha o pedaço da matriz inicial com o processo responsável //
-    if (processId == MASTER) {
-        float **initialGrid = (float **)malloc(TAMANHO * sizeof(float *));
-        inicializaGrid(initialGrid);
-        estadoInicial(initialGrid);
-        MPI_Scatterv(initialGrid, sendcounts, begin, MPI_FLOAT, recvbuffer,
-                    (nRows*TAMANHO), MPI_FLOAT, MASTER, MPI_COMM_WORLD);
-        free(initialGrid);
+  // Compartilha o pedaço da matriz inicial com o processo responsável //
+  if (procID == master) {
+    int *initialgrid = (int *) malloc(N * N * sizeof(int));
+    setInitGrid(initialgrid);
+    MPI_Scatterv(initialgrid, sendcounts, begin, MPI_INT, recvbuffer,
+                (nRows*N), MPI_INT, master, MPI_COMM_WORLD);
+    free(initialgrid);
+  } else {
+    MPI_Scatterv(NULL, sendcounts, begin, MPI_INT, recvbuffer,
+                (nRows*N), MPI_INT, master, MPI_COMM_WORLD);
+  }
+
+  int **grid = malloc((nRows + 2) * sizeof(int *));
+  int **newgrid = malloc((nRows + 2) * sizeof(int *));
+
+  for (i = 0; i < nRows+2; i++) {
+    grid[i] = malloc(N * sizeof(int));
+    newgrid[i] = malloc(N * sizeof(int));
+  }
+  zeros(newgrid, nRows+2);
+
+  join(grid, recvbuffer, nRows);
+
+  int nextProc, previousProc, finalLivingGenerations;
+  if (procID == 0) previousProc = nProcs - 1;
+  else previousProc = procID - 1;
+  nextProc = (procID + 1) % nProcs;
+
+  MPI_Request request;
+  for (generation = 0; generation < G; generation++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (generation%2 == 0) {
+      MPI_Isend(grid[1], N, MPI_INT, previousProc, 12, MPI_COMM_WORLD, &request);
+      MPI_Isend(grid[nRows], N, MPI_INT, nextProc, 12, MPI_COMM_WORLD, &request);
+      MPI_Recv(grid[nRows+1], N, MPI_INT, nextProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(grid[0], N, MPI_INT, previousProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      livingGenerations = findLivingGenerations(grid, newgrid, nRows);
     } else {
-        MPI_Scatterv(NULL, sendcounts, begin, MPI_FLOAT, recvbuffer,
-                    (nRows*TAMANHO), MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+      MPI_Isend(newgrid[1], N, MPI_INT, previousProc, 12, MPI_COMM_WORLD, &request);
+      MPI_Isend(newgrid[nRows], N, MPI_INT, nextProc, 12, MPI_COMM_WORLD, &request);
+      MPI_Recv(newgrid[nRows+1], N, MPI_INT, nextProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(newgrid[0], N, MPI_INT, previousProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      livingGenerations = findLivingGenerations(newgrid, grid, nRows);
     }
 
-    ////////////////////////////////////////////////////
-
-    float **grid = (float **)malloc(TAMANHO * sizeof(float *));
-    float **new_grid = (float **)malloc(TAMANHO * sizeof(float *));
-
-    inicializaGrid(grid);
-    inicializaGrid(new_grid);
-    estadoInicial(grid);
-
-    join(grid, recvbuffer, nRows);
-
-    int nextProc, previousProc, finalLivingGenerations, livingGenerations;
-    if (processId == MASTER) previousProc = noProcesses - 1;
-    else previousProc = processId - 1;
-    nextProc = (processId + 1) % noProcesses;
-
-    MPI_Request request;
-
-    int sobreviventesGeracoes[GERACOES];
-    //printf("chegou aquiantesalgo\n\n\n\n");
-    for(int i = 0; i < GERACOES; i++){ 
-        MPI_Barrier(MPI_COMM_WORLD);
-        if (i % 2 == 0) {
-        MPI_Isend(grid[1], TAMANHO, MPI_INT, previousProc, 12, MPI_COMM_WORLD, &request);
-        MPI_Isend(grid[nRows], TAMANHO, MPI_INT, nextProc, 12, MPI_COMM_WORLD, &request);
-        MPI_Recv(grid[nRows+1], TAMANHO, MPI_INT, nextProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(grid[0], TAMANHO, MPI_INT, previousProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        livingGenerations = findLivingGenerations(grid, new_grid, nRows);
-        } else {
-        MPI_Isend(new_grid[1], TAMANHO, MPI_INT, previousProc, 12, MPI_COMM_WORLD, &request);
-        MPI_Isend(new_grid[nRows], TAMANHO, MPI_INT, nextProc, 12, MPI_COMM_WORLD, &request);
-        MPI_Recv(new_grid[nRows+1], TAMANHO, MPI_INT, nextProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(new_grid[0], TAMANHO, MPI_INT, previousProc, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        livingGenerations = findLivingGenerations(new_grid, grid, nRows);
-        }
-
-        if (i == GERACOES-1) {
-        MPI_Reduce(&livingGenerations, &finalLivingGenerations,
-                    1, MPI_INT, MPI_SUM, MASTER, MPI_COMM_WORLD);
-        }
-        printf("Quantidade de celulas vivas Geração %d: %d\n", i, sobreviventesGeracoes[i]);
+    if (generation == G-1) {
+      MPI_Reduce(&livingGenerations, &finalLivingGenerations,
+                 1, MPI_INT, MPI_SUM, master, MPI_COMM_WORLD);
     }
-    double stoP = MPI_Wtime();
-    if (processId == 0) {
-        printf("Tempo de execução trecho de computação das gerações: %.2fs\n", stoP - starT);
-        printf("Quantidade de celulas vivas Geração %d: %d\n", GERACOES, quantidadeCelulasVivas(new_grid));
-    }
-    for (int i = 0; i < TAMANHO; i++){
-        free(grid[i]);
-        free(new_grid[i]);
-    }
-    free(grid);
-    free(new_grid);
-    MPI_Finalize();
-    return 0;
+  }
+
+  double tempoFinal = MPI_Wtime(); //Tempo do término da execução do programa
+
+  if (procID == master) {
+    printf("Tempo de execução: %.04fs\n", (tempoFinal - tempoInicio));
+    printf("Número de sobreviventes na geração %d: %d\n", G, finalLivingGenerations);
+  }
+
+  free(grid);
+  free(newgrid);
+  MPI_Finalize();
 }
 
 /*
